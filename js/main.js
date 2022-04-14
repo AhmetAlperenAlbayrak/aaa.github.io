e37baf72cea72cfebd414e4e4c6ebeb8c4caf0a3
[Ahmet Albayrak] <[ahmetalperenalbayrak@gmail.com]>
2022-04-15 02:06:46 +0300

    beta version changes added

--- js/main.js
+++ js/main.js
@@ -12,41 +12,51 @@ stickyNavbar();
 // Sticky Navbar on scroll
 window.addEventListener('scroll', stickyNavbar);
 
+/* --------------------Scrollreveal Animation---------------------- */
 
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: '60px',
+});
+
+sr.reveal(".showcase-info", {delay:600});
+sr.reveal(".showcase-image", {origin:"top", delay:700});
+
+/* --------------------Countdown---------------------- */
  
  
-//  const countdown = document.querySelector('.countdown');
+ const countdown = document.querySelector('.countdown');
 
 //  Set launch date (ms)
-//  const launchDate = new Date('May 15, 2022 00:00:00').getTime();
+ const launchDate = new Date('May 15, 2022 00:00:00').getTime();
 
-//     Update every second
-//     setInterval(() => {
-//         Get todays date and time (ms)
-//         const now = new Date().getTime();
-//         Distance from now to the launch date (ms)
-//         const distance = launchDate - now;
+    // Update every second
+    setInterval(() => {
+        // Get todays date and time (ms)
+        const now = new Date().getTime();
+        // Distance from now to the launch date (ms)
+        const distance = launchDate - now;
 
-//         Time calculations for days, hours, minutes and seconds
-//         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-//         Display result
-//         countdown.innerHTML = `
-//             <div>${days}<span>Days</span></div>
-//             <div>${hours}<span>Hours</span></div>
-//             <div>${minutes}<span>Minutes</span></div>
-//             <div>${seconds}<span>Seconds</span></div>
-//         `;
+        // Display result
+        countdown.innerHTML = `
+            <div>${days}<span>Days</span></div>
+            <div>${hours}<span>Hours</span></div>
+            <div>${minutes}<span>Minutes</span></div>
+            <div>${seconds}<span>Seconds</span></div>
+        `;
 
-//         If launch date passed
-//         if (distance < 0) {
-//             Stop countdown
-//             clearInterval(x);
-//             Style and output text
-//             countdown.style.color = '#17a2b8';
-//             countdown.innerHTML = 'Launched!';
-//         }
-//     });
+        // If launch date passed
+        if (distance < 0) {
+            // Stop countdown
+            clearInterval(x);
+            // Style and output text
+            countdown.style.color = '#17a2b8';
+            countdown.innerHTML = 'Launched!';
+        }
+    });
